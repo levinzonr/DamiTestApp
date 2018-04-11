c10255c241f388aafebd0d72a90ba239c26c6aae
Roman Levinzon <levinzon.roman@gmail.com>
2018-04-11 16:48:11 +0200

    Map fragment set-up

--- app/src/main/java/cz/levinzonr/damiapp/view/map/MapsFragment.kt
+++ app/src/main/java/cz/levinzonr/damiapp/view/map/MapsFragment.kt
@@ -3,18 +3,42 @@ package cz.levinzonr.damiapp.view.map
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
 import cz.levinzonr.damiapp.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.LatLng
 
-class MapsFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+
+class MapsFragment : SupportMapFragment(), OnMapReadyCallback{
+
+    private lateinit var map: GoogleMap
+
+    companion object {
+        const val TAG = "Maps fragment"
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        if (map != null) {
+            this.map = map
+            Log.d(TAG, "Map loaded")
+        }
+        val sydney = LatLng(-34.0, 151.0)
+        map?.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
 
