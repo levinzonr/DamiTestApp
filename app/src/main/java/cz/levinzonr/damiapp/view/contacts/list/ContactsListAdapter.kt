104ee21952c5a04ff4cdae2721e47940d1d1bbd2
Roman Levinzon <levinzon.roman@gmail.com>
2018-04-18 17:17:34 +0200

    CircleImageView dep added

--- app/src/main/java/cz/levinzonr/damiapp/view/contacts/list/ContactsListAdapter.kt
+++ app/src/main/java/cz/levinzonr/damiapp/view/contacts/list/ContactsListAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
 import cz.levinzonr.damiapp.R
 import cz.levinzonr.damiapp.model.entities.Contact
 import cz.levinzonr.damiapp.presenter.contacts.ContactsListPresenter
